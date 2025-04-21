956248e3ec728dc49dbf024966c33a706858f139
Danyl Timofeyev <timofeev.danil@gmail.com>
2025-04-21 13:51:05 +0100

    refactor: convert project to SwiftUI

---
+++ Unknown.io/App/Unknown_ioApp.swift
@@ -0,0 +1,19 @@
+//
+//  Unknown_ioApp.swift
+//  Unknown.io
+//
+//  Created by Danyl Timofeyev on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Unknown_ioApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
